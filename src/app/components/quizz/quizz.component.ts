d90ac0d42ce284acf484f5528023d3692e6e2e6a
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-02-14 22:45:30 -0300

    feat(QuizzComponent): Add customization

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+type QuestionOption = {
+  id: number,
+  name: string,
+  alias: string
+}
+
+type Question = {
+  id: number,
+  question: string,
+  options: Array<QuestionOption>
+};
+
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+  title: string = "Title";
+
+  selectedQuestion: Question = {id: 1, question: "Selected question", options: []};
+  questions: Array<Question> = [];
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  answers: Array<string> = [];
+  selectedAnswer: string = "Selected answer";
+  optionName: string = "Option name";
+
+  finished: boolean = false;
 
   constructor() { }
 
